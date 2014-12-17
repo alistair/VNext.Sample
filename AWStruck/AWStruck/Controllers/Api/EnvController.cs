cd98bdee95c93c40813777835a874192b8413cdc
Ondrej Zavadil <ondrej.zavadil@findly.com>
2014-12-18 12:21:34 +1300

    adding start / stop 1 instance

---
+++ AWStruck/AWStruck/Controllers/Api/EnvController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AttributeRouting.Web.Http;
+
+namespace AWStruck.Controllers.Api
+{
+  public class EnvController
+  {
+    [GET("/api/env/start")]
+    [HttpGet]
+    public string StartInstance()
+    {
+      return "Start";
+    }
+
+    [GET("/api/env/stop")]
+    [HttpGet]
+    public string StopInstance()
+    {
+      return "Stop";
+    }
+  }
+}
