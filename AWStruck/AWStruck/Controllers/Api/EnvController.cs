1e82a1b27f9fa886e02feac37e201ad5881d59c9
Ondrej Zavadil <ondrej.zavadil@findly.com>
2014-12-18 12:45:44 +1300

    no message

--- AWStruck/AWStruck/Controllers/Api/EnvController.cs
+++ AWStruck/AWStruck/Controllers/Api/EnvController.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Http;
 using AttributeRouting.Web.Http;
 
 namespace AWStruck.Controllers.Api
 {
-  public class EnvController : Controller
+  public class EnvController : ApiController
   {
     [GET("/api/env/start")]
     [HttpGet]
