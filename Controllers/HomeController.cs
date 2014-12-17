c753511b3915535103e17a1078bc74d711e81562
Alistair Bush <alistair@findly.com>
2014-12-18 00:28:24 +1300

    Initial commit.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNet.Mvc;
+using MvcSample.Web.Models;
+
+namespace MvcSample.Web
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(User());
+        }
+
+        public User User()
+        {
+            User user = new User()
+            {
+                Name = "Im Alistairrrrr",
+                Address = "My address"
+            };
+
+            return user;
+        }
+    }
+}
