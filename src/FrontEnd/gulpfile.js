47e4f8b05baef75462de0b6510f4fb6997a898e0
Tim C <tim.coenradi@findly.com>
2014-12-18 10:14:33 +1300

    Add Front end files

---
+++ src/FrontEnd/gulpfile.js
@@ -0,0 +1,128 @@
+'use strict';
+
+var gulp = require('gulp');
+var del = require('del');
+
+// Load plugins
+var $ = require('gulp-load-plugins')();
+var browserify = require('browserify');
+var watchify = require('watchify');
+var source = require('vinyl-source-stream'),
+    sourceFile = './app/scripts/app.js',
+    destFolder = './dist/scripts',
+    destFileName = 'app.js';
+
+// Scripts
+gulp.task('scripts', function () {
+    var bundler = watchify(browserify({
+        entries: [sourceFile],
+        insertGlobals: true,
+        cache: {},
+        packageCache: {},
+        fullPaths: true
+    }));
+
+    bundler.on('update', rebundle);
+
+    function rebundle() {
+        return bundler.bundle()
+            // log errors if they happen
+            .on('error', $.util.log.bind($.util, 'Browserify Error'))
+            .pipe(source(destFileName))
+            .pipe(gulp.dest(destFolder));
+    }
+
+    return rebundle();
+});
+
+
+
+
+// HTML
+gulp.task('html', function () {
+    return gulp.src('app/*.html')
+        .pipe($.useref())
+        .pipe(gulp.dest('dist'))
+        .pipe($.size());
+});
+
+// Images
+gulp.task('images', function () {
+    return gulp.src('app/images/**/*')
+        .pipe($.cache($.imagemin({
+            optimizationLevel: 3,
+            progressive: true,
+            interlaced: true
+        })))
+        .pipe(gulp.dest('dist/images'))
+        .pipe($.size());
+});
+
+// HTML
+gulp.task('css', function () {
+    return gulp.src('app/styles/*.css')
+        .pipe($.useref())
+        .pipe(gulp.dest('dist/styles'))
+        .pipe($.size());
+});
+
+
+
+// Clean
+gulp.task('clean', function (cb) {
+    del(['dist/styles', 'dist/scripts', 'dist/images'], cb);
+});
+
+
+// Bundle
+gulp.task('bundle', ['scripts', 'bower'], function(){
+    return gulp.src('./app/*.html')
+               .pipe($.useref.assets())
+               .pipe($.useref.restore())
+               .pipe($.useref())
+               .pipe(gulp.dest('dist'));
+});
+
+// Build
+gulp.task('build', ['html', 'bundle', 'images', 'css']);
+
+// Default task
+gulp.task('default', ['clean', 'build']);
+
+// Webserver
+gulp.task('serve', function () {
+    gulp.src('./dist')
+        .pipe($.webserver({
+            livereload: true,
+            port: 9000
+        }));
+});
+
+// Bower helper
+gulp.task('bower', function() {
+    gulp.src('app/bower_components/**/*.js', {base: 'app/bower_components'})
+        .pipe(gulp.dest('dist/bower_components/'));
+
+});
+
+gulp.task('json', function() {
+    gulp.src('app/scripts/json/**/*.json', {base: 'app/scripts'})
+        .pipe(gulp.dest('dist/scripts/'));
+});
+
+
+// Watch
+gulp.task('watch', ['html', 'bundle', 'serve', 'css'], function () {
+
+    // Watch .json files
+    gulp.watch('app/scripts/**/*.json', ['json']);
+
+    // Watch .html files
+    gulp.watch('app/*.html', ['html']);
+
+    // Watch image files
+    gulp.watch('app/images/**/*', ['images']);
+
+    // Watch css files
+    gulp.watch('app/styles/*.css', ['css']);
+});
