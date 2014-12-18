e8da6719017245ffa1a3ae07d9665357f818fa62
Stefan Mostert <stefan.mostert@findly.com>
2014-12-19 12:37:53 +1300

    Cleanup

--- AWStruck/AWStruck/Savings/SavingsHelper.cs
+++ AWStruck/AWStruck/Savings/SavingsHelper.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Drawing.Imaging;
 using System.Linq;
-using System.Web;
 using AWStruck.AWS;
 
 namespace AWStruck.Savings
@@ -15,70 +13,43 @@ namespace AWStruck.Savings
         private const double T2MediumPerHour = 0.072;
         private const double DefaultPerHour = 0.05;
 
-        private DateTime _fromDate
-        {
-            get
-            {
-                if (String.IsNullOrEmpty(ConfigurationManager.AppSettings["SavingsStartDate"])) return DateTime.Now;
-                try
-                {
-                    return DateTime.Parse(ConfigurationManager.AppSettings["SavingsStartDate"]);
-                }
-                catch
-                {
-                    return DateTime.Now;
-                }
-                return DateTime.Now;
-            }
-        }
+        //private DateTime _fromDate
+        //{
+        //    get
+        //    {
+        //        if (String.IsNullOrEmpty(ConfigurationManager.AppSettings["SavingsStartDate"])) return DateTime.Now;
+        //        try
+        //        {
+        //            return DateTime.Parse(ConfigurationManager.AppSettings["SavingsStartDate"]);
+        //        }
+        //        catch
+        //        {
+        //            return DateTime.Now;
+        //        }
+        //    }
+        //}
 
         public SavingsResponse CalculateSavings()
         {
             return new SavingsResponse
             {
-                FromDate = _fromDate,
-                TotalSaved = 2323.21,
-                CurrentSavingsPerHour = CalculateSavingsPerHour(),
-                SavedInLastDay = 211.08,
-                SavedInLastHour = 12.87
+                CurrentSavingsPerHour = CalculateSavingsPerHour()
+                //FromDate = _fromDate,
+                //TotalSaved = 2323.21,
+                //SavedInLastDay = 211.08,
+                //SavedInLastHour = 12.87
             };
         }
 
         private double CalculateSavingsPerHour()
         {
-            var savedPerHour = 0.0;
             var envs = GetTurnedOffEnvironments();
-            foreach (var environment in envs)
-            {
-                var numberOfInstances = environment.InstanceCount;
-                var price = GetPricePerHourForInstanceType(environment.Type);
-                var saved = numberOfInstances*price;
-                savedPerHour = savedPerHour + saved;
-            }
             return envs.Sum(environment => environment.InstanceCount*GetPricePerHourForInstanceType(environment.Type));
         }
 
         private IEnumerable<Environment> GetTurnedOffEnvironments()
         {
-            var env1 = new Environment
-            {
-                InstanceIds = {"instance1", "instance2", "instance3"},
-                Name = "Env1",
-                State = "running",
-                Type = "t2.micro"
-            };
-
-            var env2 = new Environment
-            {
-                InstanceIds = { "instance21", "instance22", "instance23", "instance24" },
-                Name = "Env2",
-                State = "stopped",
-                Type = "t2.micro"
-            };
-
-            var envs = new List<Environment> {env1, env2};
-
-            //var envs = Environments.GetEnvironments(Global.CreateAmazonClient());
+            var envs = Environments.GetEnvironments(Global.CreateAmazonClient());
             return envs.Where(e => e.State != "running").ToList();
         }
 
--- AWStruck/AWStruck/Savings/SavingsResponse.cs
+++ AWStruck/AWStruck/Savings/SavingsResponse.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace AWStruck.Savings
+﻿namespace AWStruck.Savings
 {
     public class SavingsResponse
     {
-        public DateTime FromDate { get; set; }
-        public double TotalSaved { get; set; }
         public double CurrentSavingsPerHour { get; set; }
-        public double SavedInLastDay { get; set; }
-        public double SavedInLastHour { get; set; }
+        //public DateTime FromDate { get; set; }
+        //public double TotalSaved { get; set; }
+        //public double SavedInLastDay { get; set; }
+        //public double SavedInLastHour { get; set; }
     }
 }
