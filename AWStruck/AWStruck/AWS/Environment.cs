b9cf810a4efa0e8924c7ed470e27d700d78a4e1e
Juan <juan.herbst@findly.com>
2014-12-19 11:15:17 +1300

    return a list of environments when client/server SignalRrrrr connection is established

--- AWStruck/AWStruck/AWS/Environment.cs
+++ AWStruck/AWStruck/AWS/Environment.cs
@@ -3,34 +3,35 @@ using System.Linq;
 
 namespace AWStruck
 {
-	public class Environment
-	{
-		public Environment()
-		{
-			InstanceIds = new List<string>();
-		}
+    public class Environment
+    {
+        public Environment()
+        {
+            InstanceIds = new List<string>();
+        }
 
-		public string Name { get; set; }
-		
+        public string Name { get; set; }
+
 
         public int InstanceCount
         {
             get { return InstanceIds.Count(); }
         }
 
-		public List<string> InstanceIds { get; set; }
-    
-    public bool IsAuto { get; set; }
+        public List<string> InstanceIds { get; set; }
 
-	  public Environment CloneWithAuto(bool auto)
-	  {
-	    return new Environment()
-	    {
-	      Name = Name,
-	      InstanceIds = InstanceIds,
-	      IsAuto = auto
-	    };
-	  }
+        public bool IsAuto { get; set; }
+
         public string State { get; set; }
-	}
+
+        public Environment CloneWithAuto(bool auto)
+        {
+            return new Environment
+            {
+                Name = Name,
+                InstanceIds = InstanceIds,
+                IsAuto = auto
+            };
+        }
+    }
 }
--- AWStruck/AWStruck/Hubs/SwitchHub.cs
+++ AWStruck/AWStruck/Hubs/SwitchHub.cs
@@ -1,13 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Threading;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 using AWStruck.AWS;
 using AWStruck.Models;
+using AWStruck.Mongo;
 
-using Microsoft.Ajax.Utilities;
 using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
+
+using MongoDB.Driver.Linq;
 
 namespace AWStruck.Hubs
 {
@@ -38,6 +40,21 @@ namespace AWStruck.Hubs
                     Id = envId,
                     Status = "Started"
                 });
+        }
+
+        public override Task OnConnected()
+        {
+            IEnumerable<Environment> environments = Environments.GetEnvironments(Global.CreateAmazonClient()).Select(x => x.CloneWithAuto(IsAuto(x.Name)));
+            Clients.All.signal(environments);
+
+            return base.OnConnected();
+        }
+
+        private bool IsAuto(string envId)
+        {
+            return MongoProvider.Database.Value.GetCollection("hangfire.set")
+                .AsQueryable()
+                .Count(x => ((string)x["Key"]).StartsWith(string.Format("recurring-job:{0}", envId))) > 0;
         }
     }
 }
