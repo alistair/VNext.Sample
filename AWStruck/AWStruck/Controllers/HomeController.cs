dbd201bcf5bff05873f06292c6021c81ff23fc15
Stefan Mostert <stefan.mostert@findly.com>
2014-12-18 14:25:09 +1300

    Updated publish, removed some junk

--- AWStruck/AWStruck/Controllers/HomeController.cs
+++ AWStruck/AWStruck/Controllers/HomeController.cs
@@ -4,18 +4,12 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-using AWStruck.Hubs;
-
 namespace AWStruck.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            var booboo = new HublyBubly();
-
-            booboo.Hello("world");
-
             return View();
         }
     }
