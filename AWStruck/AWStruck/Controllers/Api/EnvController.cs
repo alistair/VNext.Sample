7641064e7cfbcc0e1cdc46d09fffc4c7fb91bc64
Juan <juan.herbst@findly.com>
2014-12-19 13:19:54 +1300

    removed service from action, updated the shutdown times

--- AWStruck/AWStruck/Controllers/Api/EnvController.cs
+++ AWStruck/AWStruck/Controllers/Api/EnvController.cs
@@ -1,64 +1,55 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-using System;
 using System.Web.Http;
 
-using Amazon.EC2.Model;
-
 using AWStruck.AWS;
-using AWStruck.Mongo;
 using AWStruck.Hubs;
-using AWStruck.Services;
-using Hangfire;
-using MongoDB.Driver;
-using MongoDB.Driver.Linq;
-
+using AWStruck.Models;
+using AWStruck.Mongo;
 using AWStruck.Savings;
+
+using Hangfire;
 
 using Microsoft.AspNet.SignalR;
 
-using InstanceStatus = AWStruck.Models.InstanceStatus;
+using MongoDB.Bson;
+using MongoDB.Driver.Linq;
 
 namespace AWStruck.Controllers.Api
 {
-  public class EnvController : ApiController
-  {
+    public class EnvController : ApiController
+    {
         protected readonly Lazy<IHubContext> SwitchHub = new Lazy<IHubContext>(() => GlobalHost.ConnectionManager.GetHubContext<SwitchHub>());
-        private readonly EnvService _envService;
 
-    public EnvController()
-    {
-      _envService = new EnvService();
-    }
+        [HttpGet]
+        public IEnumerable<Environment> Index()
+        {
+            return Environments.GetEnvironments(Global.CreateAmazonClient()).Select(Map);
+        }
 
-    [HttpGet]
-    public IEnumerable<Environment> Index()
-    {
-      return Environments.GetEnvironments(Global.CreateAmazonClient()).Select(Map);
-    }
+        private Environment Map(Environment env)
+        {
+            List<BsonDocument> results = MongoProvider.Database.Value.GetCollection("hangfire.hash")
+                .AsQueryable()
+                .Where(x => ((string)x["Key"]).StartsWith(string.Format("recurring-job:{0}", env.Name)) && x["Field"] == "Cron")
+                .ToList();
 
-    private Environment Map(Environment env)
-    {
-      var results = MongoProvider.Database.Value.GetCollection("hangfire.hash")
-        .AsQueryable()
-        .Where(x => ((string) x["Key"]).StartsWith(string.Format("recurring-job:{0}", env.Name)) && x["Field"] == "Cron")
-        .ToList();
+            CronDescription[] desc = results.Select(
+                x => new CronDescription
+                {
+                    Name = x["Key"].AsString.Split('_')[1],
+                    Description = Cron.GetDescription((string)x["Value"])
+                }).ToArray();
 
-      var desc = results.Select(x => new CronDescription
-      {
-        Name = x["Key"].AsString.Split('_')[1],
-        Description = Cron.GetDescription((string)x["Value"])
-      }).ToArray();
+            return env.CloneWithAutoAndDescriptions(results.Any(), desc);
+        }
 
-      return env.CloneWithAutoAndDescriptions(results.Any(), desc);
-    }
-
-    [HttpGet]
+        [HttpGet]
         [Route("api/env/nogo/{envId}")]
         public string StopEnv([FromUri] string envId)
-    {
-      Environments.StopEnvironmentInternal(envId);
+        {
+            Environments.StopEnvironmentInternal(envId);
 
             SwitchHub.Value.Clients.All.signal(
                 new InstanceStatus
@@ -67,14 +58,14 @@ namespace AWStruck.Controllers.Api
                     Status = "Stopped"
                 });
 
-      return "done";
-    }
+            return "done";
+        }
 
-    [HttpGet]
+        [HttpGet]
         [Route("api/env/go/{envId}")]
         public string StartEnv([FromUri] string envId)
-    {
-      Environments.StartEnvironmentInternal(envId);
+        {
+            Environments.StartEnvironmentInternal(envId);
 
             SwitchHub.Value.Clients.All.signal(
                 new InstanceStatus
@@ -82,40 +73,23 @@ namespace AWStruck.Controllers.Api
                     Id = envId,
                     Status = "Started"
                 });
-      return "done";
-    }
-
-    [HttpGet]
-    [Route("api/env/createSchedule")]
-    public string CreateEnvironment([FromUri] string envId)
-    {
-      RecurringJob.AddOrUpdate(string.Format("{0}_start", envId), Environments.StartEnvironment(envId), "5 * * * *");
-      RecurringJob.AddOrUpdate(string.Format("{0}_stop", envId), Environments.StopEnvironment(envId), "5 * * * *");
-      return "done";
-    }
-
-        //Deprecated
-        [Route("api/env/start/")]
-    [HttpGet]
-        public StartInstancesResponse StartInstance()
-    {
-            return _envService.Start();
-    }
-
-        //Deprecated
-    [Route("api/env/stop")]
-    [HttpGet]
-        public StopInstancesResponse StopInstance()
-        {
-            return _envService.Stop();
+            return "done";
         }
 
+        [HttpGet]
+        [Route("api/env/createSchedule")]
+        public string CreateEnvironment([FromUri] string envId)
+        {
+            RecurringJob.AddOrUpdate(string.Format("{0}_start", envId), Environments.StartEnvironment(envId), "0 5 * * 1,2,3,4,5");
+            RecurringJob.AddOrUpdate(string.Format("{0}_stop", envId), Environments.StopEnvironment(envId), "0 18 * * 1,2,3,4,5");
+            return "done";
+        }
 
         [Route("api/savings")]
         [HttpGet]
         public SavingsResponse GetSavings()
-    {
+        {
             return SavingsHelper.CalculateSavings();
+        }
     }
-  }
 }
